4dd6bef160d4824655e9932fabc046e71043b1f5
Nadeem Jabbar Qureshi <nadeemjq@gmail.com>
2016-04-14 15:53:49 +0200

    Update package details

--- package.js
+++ package.js
@@ -1,23 +1,23 @@
-Package.describe({
-  name: 'acemtp:accounts-passwordless',
-  version: '0.2.3',
-  summary: 'Token-based one-time password (OTPW) authentication (nopassword, passwordless)',
-  git: 'https://github.com/efounders/meteor-accounts-passwordless',
-  documentation: 'README.md'
-});
+Package.describe( {
+	name: 'nadeemjq:accounts-passwordless',
+	version: '0.2.3',
+	summary: 'TEMPORARY FORK!!! Token-based one-time password (OTPW) authentication (nopassword, passwordless)',
+	git: 'https://github.com/nadeemja/meteor-accounts-passwordless',
+	documentation: 'README.md'
+} );
 
-Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.2');
+Package.onUse( function ( api ) {
+	api.versionsFrom( 'METEOR@0.9.2' );
 
-  api.use(['tracker', 'underscore', 'templating', 'session'], 'client');
-  api.use('email', 'server');
-  api.use(['accounts-base', 'check'], ['client', 'server']);
+	api.use( [ 'tracker', 'underscore', 'templating', 'session' ], 'client' );
+	api.use( 'email', 'server' );
+	api.use( [ 'accounts-base', 'check' ], [ 'client', 'server' ] );
 
-  // Export Accounts (etc) to packages using this one.
-  api.imply('accounts-base', ['client', 'server']);
+	// Export Accounts (etc) to packages using this one.
+	api.imply( 'accounts-base', [ 'client', 'server' ] );
 
-  api.use(['random', 'jperl:match-ex@1.0.0'], 'server');
+	api.use( [ 'random', 'jperl:match-ex@1.0.0' ], 'server' );
 
-  api.addFiles(['accounts-passwordless-ui.html', 'accounts-passwordless-ui.js'], 'client');
-  api.addFiles('accounts-passwordless.js');
-});
+	api.addFiles( [ 'accounts-passwordless-ui.html', 'accounts-passwordless-ui.js' ], 'client' );
+	api.addFiles( 'accounts-passwordless.js' );
+} );
